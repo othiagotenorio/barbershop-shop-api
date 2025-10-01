629a8b68deadf78158a579d68d55ec155c79fb54
othiagotenorio2 <thiago.tenorio@cotefacil.com>
2025-10-01 14:32:20 -0300

    Primeiro commit: Projeto inicial

---
+++ src/main/java/com/app/barbeariashop/BarbeariashopApplication.java
@@ -0,0 +1,13 @@
+package com.app.barbeariashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariashopApplication.class, args);
+	}
+
+}
